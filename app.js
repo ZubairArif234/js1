073192bd8ed50106e1c8571e74a5e350765452a5
ZubairArif234 <104222183+ZubairArif234@users.noreply.github.com>
2022-05-13 01:21:02 -0700

    JS assignment completed
    JS assignment completed

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("Error! plese enter a valid passward")
+alert("Welcome to JS land... \r\n Happy Coding!")
+alert("Welcome to JS land...")
+alert("Hello... I can run JS through my web browser's console")
---
+++ apps.js
@@ -0,0 +1,28 @@
+var variable = "HELLO WORLD"
+alert (variable);
+
+var username = "ZUBAIR ARIF"
+alert (username) ;
+
+var age ="17 years old"
+alert (age);
+
+var certification = "Certified Mobile App Development"
+alert (certification)
+
+var pizza = "pizza" + '\r\n' + "pizz" + '\r\n' + "piz" + '\r\n' + "pi" + '\r\n' + "p"
+alert (pizza)
+
+var email = " zubarif234@gmail.com"
+alert ( "my email address is" + email)
+
+var book = " A smarter way to learn JavaScript."
+alert ("I am trying to learn from the book" + book)
+
+document.write ("YAH ! I can write HTML content through JavaScript")
+
+
+
+
+
+
